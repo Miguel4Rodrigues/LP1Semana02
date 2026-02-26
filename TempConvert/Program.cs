6a168643f2c5f73d8f0b409a8e1bb55c8887c158
Miguel Rodrigues <miguel4rodrigues@gmail.com>
2026-02-26 16:54:18 +0000

    Add Unit Conversion

--- TempConvert/Program.cs
+++ TempConvert/Program.cs
@@ -10,8 +10,26 @@ namespace TempConvert
             CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
             Console.Write("Temperature: ");
             double Temp = double.Parse(Console.ReadLine());
-            Console.Write("Unit: ");
+            Console.Write("Unit (C/F): ");
             string Unit = Console.ReadLine();
+            string response;
+        
+            switch (Unit)
+            {
+                case "C":
+                    double Fahrenheit = Temp * 1.8 + 32;
+                    response = $"{Temp:f2} C = {Fahrenheit:f2} F";
+                    Console.WriteLine(response);
+                    break;
+
+                case "F":
+                    double Celsius = (Temp - 32) / 1.8;
+                    response = $"{Temp:f2} F = {Celsius:f2} C";
+                    Console.WriteLine(response);
+                    break;
+            }
+
+
         }
     }
 }
